2c0042ebd9043cc9711f7aefe20ecf0d3bc8f90e
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-03-03 19:01:33 -0500

    caught up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,16 +5,46 @@ class ArticlesController < ApplicationController
   end
 
   def index
+    # grabs and displays all articles in article path
     @articles = Article.all
   end
 
   def new
+    # when you create a new article it prevents the loop in controller from throwing an error on first iteration
+    @article = Article.new
+  end
 
+  def edit
+    # similar to show, finds the article you're editing by id
+    @article = Article.find(params[:id])
   end
 
   def create
+    # saves created article the redirects you to article show page, white list
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if  @article.save
+      flash[:notice] = "Article was created sucessfully."
+      redirect_to @article
+    else
+      render 'new'
+   end
+  end
+
+  def update
+    # updates the current article by id, white list
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was created sucessfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # shit blows up and you go to the articles page
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # the routes, this comment doesn't even seem nesseccary
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create]
+
+  # don't have to show 'only' because all routes are exposed.
+  resources :articles
 end

aba0f482eb0f4e4f608695cdb57128c55ade3232
jakeNorman007 <jacob.a.norman91@gmail.com>
2023-03-21 10:41:48 -0400

    forgot to commit categories integration test

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: "Sports")
+    @categoryTwo = Category.create(name: "Travel")
+  end
+
+  test "should show category listings" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@categoryTwo), text: @categoryTwo.name
+  end
+end
